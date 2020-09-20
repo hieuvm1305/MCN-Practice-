f5bfc17237ec4e5cf27582def34bd7595dfa5b7f
hieuvm1305 <61863560+hieuvm1305@users.noreply.github.com>
2020-09-20 18:16:37 +0700

    Update negbinomial.py

--- negbinomial.py
+++ negbinomial.py
@@ -1,15 +1,38 @@
-def prob():
+import math
+def gt(n):
+    tich=1;
+    for i in range(1,n+1):
+        tich*=i
+    return tich
 
-def infoMeasure():
+def prob(n,p,r):
+     x=gt(n)
+     y=gt(r)
+     z=gt(r-n)
+     xs=(r/(y*z)*1/(2**n))
+     return xs
 
-def sumProb():
+
+def infoMeasure(n,p,r):
+    k = -(math.log2(prob(n, p, r)))
+    return k
+
+def sumProb(N,p,r):
     '''
-
-    :return:
+    created by VuMinhHieu
     '''
+    sum = 0
+    for i in range(1, N + 1):
+        sum += prob(i, p, r)
+    return sum
 
-def approxEntropy():
+def approxEntropy(N,p,r):
     '''
-
-    :return:
+    hello world,python!
+    ham tinh trung binh luong tin
     '''
+    avg = 0
+    for i in range(1, N + 1):
+        avg += infoMeasure(i, p, r)
+    avg = avg / N
+    return avg
