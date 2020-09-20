b99dec680eddbd0269ec833914e14710e34b0c2b
hieuvm1305 <61863560+hieuvm1305@users.noreply.github.com>
2020-09-20 17:58:29 +0700

    Update binomial.py

--- binomial.py
+++ binomial.py
@@ -1,28 +1,36 @@
 import math
 def gt(n):
-    sum=1;
+    tich=1;
     for i in range(1,n+1):
-        sum*=i
-    return sum;
+        tich*=i
+    return tich;
 
 def prob(n,p,N):
-    x=gt(n)
-    y=gt(p)
-    z=gt(n-p)
-    t=float((x/(y*z))*(1/(2**n)))
-    return t
+    x=gt(N)
+    y=gt(n)
+    z=gt(N-n)
+    p=float((x/(y*z))*(1/(2**n)))
+    return p
 
 def infoMeasure(n,p,N):
-    k=-(math.log2(prob(n,p)))
+    k=-(math.log2(prob(n,p,N)))
     return k
 def sumProb(n,p,N):
     '''
-
-
+    created by VuMinhHieu_18020513
+    ham tinh tong xac suat
     '''
-    sum1=0
+    sum=0
+    for i in range(1, N + 1):
+        sum+=prob(i,p,N)
+    return sum
 
 def approxEntropy(n,p,N):
     '''
-
+    tinh trung binh luong tin
     '''
+    avg=0
+    for i in range(1,N+1):
+        avg+=infoMeasure(i,p,N)
+    avg=avg/N
+    return avg
