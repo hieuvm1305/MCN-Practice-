2c6380b7d7971f98b54bf660b1f02fd9ff63e441
hieuvm1305 <61863560+hieuvm1305@users.noreply.github.com>
2020-09-20 18:28:42 +0700

    Update negbinomial.py

--- negbinomial.py
+++ negbinomial.py
@@ -1,16 +1,8 @@
 import math
-def gt(n):
-    tich=1;
-    for i in range(1,n+1):
-        tich*=i
-    return tich
 
 def prob(n,p,r):
-     x=gt(n)
-     y=gt(r)
-     z=gt(r-n)
-     xs=(r/(y*z)*1/(2**n))
-     return xs
+     xs = math.factorial(n - 1) / (math.factorial(n - r) * math.factorial(r - 1))
+    return xs * (p ** r) * ((1 - p) ** (n - r))
 
 
 def infoMeasure(n,p,r):
@@ -18,21 +10,29 @@ def infoMeasure(n,p,r):
     return k
 
 def sumProb(N,p,r):
+    
+    sum = 0
+    for i in range(r, N + 1):
+        sum += prob(i, p, r)
+    return sum
     '''
     created by VuMinhHieu
     '''
-    sum = 0
-    for i in range(1, N + 1):
-        sum += prob(i, p, r)
-    return sum
+    ''' sumProb with N = 5, r = 2, p = 0.5:  0.8125 '''
+    ''' sumProb with N = 10, r = 2, p = 0.5:  0.9892578125 '''
+    ''' sumProb with N = 20, r = 2, p = 0.5:  0.9999799728393555 '''
 
 def approxEntropy(N,p,r):
     '''
-    hello world,python!
     ham tinh trung binh luong tin
     '''
     avg = 0
-    for i in range(1, N + 1):
-        avg += infoMeasure(i, p, r)
-    avg = avg / N
-    return avg
+    for i in range(r, N + 1):
+        avg+= prob(i, p, r) * infoMeasure(i, p, r)
+    return avg 
+'''
+ham tinh trung binh luong tin 
+'''
+''' entropy with N = 10, r = 2, p = 0.5:  2.6178901263724024 '''
+''' entropy with N = 20, r = 2, p = 0.5:  2.7111142477765284 '''
+''' entropy with N = 50, r = 2, p = 0.5: 2.7114687242185123 '''
